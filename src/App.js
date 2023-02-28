c2ababa504363edeb043954be70acf1384dfb8fa
Katja Lusma <katja.lusma@gmail.com>
2023-02-28 09:55:48 +0200

    2.12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setFetchedCountries(response.data);
+    });
+  }, []);
+
+  const handleCountryFilter = (event) => {
+    setSearch(event.target.value);
+  };
+
+  // TODO empty list when no filtered? instead of all the names. useState with empty array?
+
+  const countryNames = fetchedCountries.map((c) => c.name?.common);
+
+  const filteredCountries = countryNames.filter((n) =>
+    n.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="search">
+          <label for="countrysearch">Hae?</label>
+          <input name="countrysearch" onChange={handleCountryFilter}></input>
+        </div>
       </header>
+      <body>
+        <div className="countries">
+          {filteredCountries.map((c) => (
+            <div>{c}</div>
+          ))}
+        </div>
+      </body>
     </div>
   );
 }
