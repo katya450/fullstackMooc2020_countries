b5e7febbfdb1e9ed10faa7b48289ac4ed2d8c772
katja lusma <katja.lusma@siili.com>
2023-03-01 12:34:56 +0200

    2.13. all included

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
-function App() {
+const App = () => {
   const [fetchedCountries, setFetchedCountries] = useState([]);
   const [search, setSearch] = useState("");
+  const [countryToShow, setCountryToShow] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState("");
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
@@ -16,12 +18,16 @@ function App() {
     setSearch(event.target.value);
   };
 
+  const handleCountryClick = (countryName) => {
+    setSelectedCountry(countryName);
+  };
+
   const countryNames = fetchedCountries.map((c) => c.name?.common);
 
   const filteredCountries = () =>
     countryNames.filter((n) => n.toLowerCase().includes(search.toLowerCase()));
 
-  const countriesToShow = search === "" ? [] : filteredCountries();
+  const countryList = search === "" ? [] : filteredCountries();
 
   const countryInfo = (countryName) => {
     const countryData = fetchedCountries.find(
@@ -50,10 +56,18 @@ function App() {
     );
   };
 
+  useEffect(() => {
+    setCountryToShow(selectedCountry);
+  }, [selectedCountry]);
+
   const countries =
-    countriesToShow.length === 1
-      ? countryInfo(countriesToShow[0])
-      : countriesToShow.map((c) => <div>{c}</div>);
+    countryList.length === 1
+      ? countryInfo(countryList[0])
+      : countryList.map((c) => (
+          <div key={c}>
+            {c} <button onClick={() => handleCountryClick(c)}>KLIK</button>
+          </div>
+        ));
 
   return (
     <div className="App">
@@ -65,15 +79,18 @@ function App() {
       </header>
       <body>
         <div className="countries">
-          {countriesToShow.length > 10 ? (
+          {countryList.length > 10 ? (
             <div>Rajaa enemmän, liikaa tuloksia (yli 10)</div>
           ) : (
             countries
+          )}
+          {countryList.length !== 1 && countryToShow !== "" && (
+            <div>{countryInfo(selectedCountry)}</div>
           )}
         </div>
       </body>
     </div>
   );
-}
+};
 
 export default App;
